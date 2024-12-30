d7281b428acd2ad06b7363cd636ce80400c14223
milindkusahu <milindsahu2000@gmail.com>
2024-12-30 21:21:59 +0530

    About section component

---
+++ src/components/About.jsx
@@ -0,0 +1,56 @@
+const aboutItems = [
+  {
+    label: "Project done",
+    number: 45,
+  },
+  {
+    label: "Years of experience",
+    number: 10,
+  },
+];
+
+const About = () => {
+  return (
+    <section id="about" className="section">
+      <div className="container">
+        <div className="bg-zinc-800/50 p-7 rounded-2xl md:p-12 reveal-up">
+          <p className="text-zinc-300 mb-4 md:mb-8 md:text-xl md:max-w-[60ch]">
+            Welcome! I&apos;m Milind, a front-end developer specializing in
+            React JS. With expertise in modern web technologies, I focus on
+            building user-centric interfaces that combine clean code with
+            exceptional user experience. Currently working on projects that
+            optimize Core Web Vitals and implement seamless API integrations,
+            I&apos;m always excited to tackle new challenges in web development.
+          </p>
+
+          <div className="flex flex-wrap items-center gap-4 md:gap-7">
+            {aboutItems.map(({ label, number }, key) => (
+              <div key={key}>
+                <div className="flex items-center md:mb-2">
+                  <span className="text-2xl font-semibold md:text-4xl">
+                    {number}
+                  </span>
+                  <span className="text-sky-400 font-semibold md:text-3xl">
+                    +
+                  </span>
+                </div>
+
+                <p className="text-sm text-zinc-400">{label}</p>
+              </div>
+            ))}
+
+            <img
+              src="/images/logo.svg"
+              alt="Logo"
+              width={30}
+              height={30}
+              className="ml-auto md:w-[40px] md:h-[40px]"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
